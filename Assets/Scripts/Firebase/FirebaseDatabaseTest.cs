3c8d92d92bb862140131b167e50dd61f85b097e8
신은주 <slew61@naver.com>
2023-06-09 21:13:59 +0900

    ✅ Edit | Firebase 사용 테스트 코드 메뉴아이템으로 수정

--- Assets/Scripts/Firebase/FirebaseDatabaseTest.cs
+++ Assets/Scripts/Firebase/FirebaseDatabaseTest.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class FirebaseDatabaseTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [MenuItem("Firebase/Upload")]
+    static void UploadTest()
     {
         UserInfo userInfo = new UserInfo("test@naver.com","MyName");
         string serializedData = JsonUtility.ToJson(userInfo);
         FirebaseRealtimeDatabaseManager.Instance.UploadUserInfo("YGU345D2sD",serializedData);
+    }
+
+    [MenuItem("Firebase/Download")]
+    static void DownloadTest()
+    {
+        FirebaseRealtimeDatabaseManager.Instance.LoadUserInfo("YGU345D2sD",(info)=>{Debug.Log(info.email);});
     }
 
 }
