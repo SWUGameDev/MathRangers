b9fa7056eed2c78e42a35ba8c77dba2f77de020d
신은주 <slew61@naver.com>
2023-06-13 21:50:55 +0900

    🎨 Extract | 로그인과 회원가입의 Error 핸들링을 위해 함수로 추출 정리 완료

--- Assets/Scripts/Login/AuthManager.Error.cs
+++ Assets/Scripts/Login/AuthManager.Error.cs
@@ -9,43 +9,40 @@ using System.Threading.Tasks;
 
 public partial class AuthManager: MonoBehaviour {
 
-    private void LoginErrorHandler(InnerExceptions innerExceptions) {
+    private void LoginErrorHandler(System.Collections.ObjectModel.ReadOnlyCollection<System.Exception>  innerExceptions) {
         foreach(var innerException in innerExceptions) {
+                if (!(innerException is FirebaseException firebaseException))
+                    continue;
 
-            if (innerException is FirebaseException firebaseException) {
-                if ((AuthError)firebaseException.ErrorCode == AuthError.WrongPassword) {
-                    LoginUIManager
-                        .Instance
-                        .PopUpMessage("! 비밀번호를 다시 확인해주세요. ");
-                } else if ((AuthError)firebaseException.ErrorCode == AuthError.WrongPassword) {
-                    LoginUIManager
-                        .Instance
-                        .PopUpMessage("! 존재하지 않는 이메일입니다.\n회원가입을 진행해주세요.");
-                } else {
-                    Debug.LogWarning(
-                        $"{(AuthError)firebaseException.ErrorCode} SignInWithEmailAndPasswordAsync encou" +
-                        "ntered an error: {firebaseException.Message}"
-                    );
-                }
+                switch((AuthError) firebaseException.ErrorCode )
+                {
+                    case AuthError.WrongPassword : 
+                        LoginUIManager.Instance.PopUpMessage("! 비밀번호를 다시 확인해주세요. ");
+                        break;
+                    case AuthError.InvalidEmail :
+                        LoginUIManager.Instance.PopUpMessage("! 맞지 않는 아이디 형식입니다.");
+                        break;
+                    default: 
+                        LoginUIManager.Instance.PopUpMessage($"! Error Error code {firebaseException.ErrorCode}");
+                        break;        
+                }    
             }
         }
-    }
 
-    private void SignUpErrorHandler(InnerExceptions innerExceptions) {
+    private void SignUpErrorHandler(System.Collections.ObjectModel.ReadOnlyCollection<System.Exception>  innerExceptions) {
         foreach(var innerException in innerExceptions) {
-            if (innerException is FirebaseException firebaseException) {
-                if ((AuthError)firebaseException.ErrorCode == AuthError.EmailAlreadyInUse) {
-                    LoginUIManager
-                        .Instance
-                        .PopUpMessage("! 이미 회원가입 된 이메일 입니다.");
-                } else {
-                    Debug.LogWarning(
-                        $"{(AuthError)firebaseException.ErrorCode} CreateUserWithEmailAndPasswordAsync e" +
-                        "ncountered an error: " + firebaseException.Message
-                    );
-                }
-            }
-        }
-    }
+                if (!(innerException is FirebaseException firebaseException))
+                    continue;
 
+                switch((AuthError) firebaseException.ErrorCode )
+                {
+                    case AuthError.EmailAlreadyInUse : 
+                        LoginUIManager.Instance.PopUpMessage("! 이미 회원가입 된 이메일 입니다.");
+                        break;
+                    default: 
+                        LoginUIManager.Instance.PopUpMessage($"! Error Error code {firebaseException.ErrorCode}");
+                        break;    
+                }    
+            }
+    }
 }
