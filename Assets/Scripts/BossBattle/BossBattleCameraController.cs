891e560d9145f75ab5f2a04854bef9c6a01de1c6
ymthebest <ymthebest57@gmail.com>
2023-06-17 02:06:14 +0900

    🔨 Add | player를 따라서 camera 이동

---
+++ Assets/Scripts/BossBattle/BossBattleCameraController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBattleCameraController : MonoBehaviour
+{
+    public float cameraSpeed = 5.0f;
+
+    public GameObject player;
+
+    private void Update()
+    {
+        Vector3 dir = player.transform.position - this.transform.position;
+        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
+        this.transform.Translate(moveVector);
+    }
+}
