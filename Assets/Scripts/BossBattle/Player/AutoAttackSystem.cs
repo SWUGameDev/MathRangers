4e39fd70abfbe0166d606543f4d8e23efe4aea4f
Kim YeonMo <ymthebest57@gmail.com>
2023-06-27 23:05:51 +0900

    🐛 Fix | 자동 공격 시스템 구조 개선
    thanks to. 은주님

--- Assets/Scripts/BossBattle/Player/AutoAttackSystem.cs
+++ Assets/Scripts/BossBattle/Player/AutoAttackSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,41 +9,43 @@ public class AutoAttackSystem : MonoBehaviour
     public float attackInterval = 1.0f; 
 
     private ObjectPool bulletPool;
-    private GameObject player;
     private Transform firePoint;
-    private float timer; 
+    private float timer;
 
+    [SerializeField] public GameObject monster;
     private void Start()
     {
         bulletPool = new ObjectPool(bulletPrefab, 10, "BulletPool");
         firePoint = transform;
     }
 
+    
     private void Update()
     {
         timer += Time.deltaTime;
 
         if (timer >= attackInterval)
         {
-            Shoot();
+            CreateBullet();
             timer = 0f;
         }
     }
 
-    private void Shoot()
+    private void CreateBullet()
     {
         GameObject bulletObj = bulletPool.GetObject();
 
         Bullet bullet = bulletObj.GetComponent<Bullet>();
 
-        if (bullet != null)
-        {
-            bulletObj.transform.position = firePoint.position;
-            bulletObj.transform.rotation = firePoint.rotation;
-            //bullet.Fire(firePoint.up); // 플레이어의 전방으로 총알 발사
+        bullet.Initialized(this);
 
-        }
+        bulletObj.transform.position = firePoint.position;
+        bulletObj.transform.rotation = firePoint.rotation;
+        bullet.Shot();
+    }
 
-        bulletObj.SetActive(true);
+    public ObjectPool GetBulletPool()
+    {
+        return this.bulletPool;
     }
 }
--- Assets/Scripts/BossBattle/Player/Bullet.cs
+++ Assets/Scripts/BossBattle/Player/Bullet.cs
@@ -4,42 +4,50 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] public float maxDistance = 3f;
-    [SerializeField] private float bulletSpeed = 5.0f;
+    [SerializeField] public float time = 0f;
+    public float timeInterval = 1f;
+    [SerializeField] private float bulletSpeed = 13.0f;
     private Vector3 startPosition;
     Rigidbody2D rigid;
-    public GameObject monster;
 
+    private AutoAttackSystem autoAttackSystem;
 
+    // 오토액션에 대해 들고잇어야함
     private void Awake()
     {
-        rigid= GetComponent<Rigidbody2D>();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
-    private void Start()
+    public void Shot()
     {
-        monster = GameObject.Find("Boss");
-
         startPosition = transform.position;
-        Vector3 monsterDir = monster.transform.position - transform.position;
+
+        
+         Vector3 monsterDir = autoAttackSystem.monster.transform.position - transform.position;
+
+        monsterDir = monsterDir == Vector3.zero ? Vector3.up : monsterDir;
         rigid.velocity = monsterDir.normalized * bulletSpeed;
     }
 
+    public void Initialized(AutoAttackSystem autoAttackSystem)
+    {
+        this.autoAttackSystem = autoAttackSystem;
 
-
-    //public void Fire(Vector2 direction)
-    //{
-    //    rigid.velocity = direction.normalized * bulletSpeed;
-    //}
+    }
 
     private void Update()
     {
         float currentDistance = Vector3.Distance(startPosition, transform.position);
 
-        if (currentDistance >= maxDistance)
+        time += Time.deltaTime;
+        if (time >= timeInterval)
         {
+            Debug.Log("디버그");
             // 총알을 오브젝트 풀로 반환?
-            gameObject.SetActive(false);
+            // ReturnObject로 하고 싶은데.?
+            // gameObject.SetActive(false);
+            this.autoAttackSystem.GetBulletPool().ReturnObject(this.gameObject);
+            time = 0f;
         }
     }
 }
