0a621aeed3ce5902553e9cbc5abe0492f48c3f41
Kim YeonMo <ymthebest57@gmail.com>
2023-07-02 16:50:01 +0900

    🔨 Add | 플레이어 점프, 보스랑 충돌

--- Assets/Scripts/BossBattle/Player/Player.cs
+++ Assets/Scripts/BossBattle/Player/Player.cs
@@ -7,13 +7,22 @@ using UnityEngine.EventSystems;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    float playerSpeed = 10f;
+    float playerSpeed = 15f;
 
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float jumpForce = 350f;
+    private bool isJumping;
     private void Awake()
     {
         VirtualJoystick.OnProcessInput += OnProcessInput;
     }
 
+    private void Start()
+    {
+        this.rb = GetComponent<Rigidbody2D>();
+        isJumping = false;
+    }
     private void OnDestroy()
     {
         VirtualJoystick.OnProcessInput -= OnProcessInput;
@@ -22,5 +31,47 @@ public class Player : MonoBehaviour
     private void OnProcessInput(Vector2 vdir)
     {
         transform.position += new Vector3(vdir.x, 0f, 0f) * playerSpeed * Time.deltaTime;
+    }
+
+    public void Jump()
+    {
+        if (isJumping == false)
+        {
+            isJumping = true;
+            this.rb.AddForce(transform.up * this.jumpForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    { 
+        if (col.transform.name == "Ground") 
+        { 
+            isJumping = false; 
+        }
+
+        Debug.Log("콜리전");
+
+    }
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("콜리전 스테이");
+        if (collision.transform.name == "Boss")
+        {
+            Debug.Log("보z스");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("보스");
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("보스 닿는중");
+
     }
 }
