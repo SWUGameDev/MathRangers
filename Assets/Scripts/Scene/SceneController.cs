3a5c09caf78a3439db350550814b5158cf2c1c7a
신은주 <slew61@naver.com>
2023-07-04 23:29:45 +0900

    🔨 Add | MainScene 내 Scene 이동 로직 추가 (ShopScene & BossScene)

--- Assets/Scripts/Scene/SceneController.cs
+++ Assets/Scripts/Scene/SceneController.cs
@@ -39,4 +39,14 @@ public class SceneController : MonoBehaviour
     {
         SceneManager.LoadScene("03_SignupScene");
     }
+
+    public void LoaBossScene()
+    {
+        SceneManager.LoadScene("07_BossScene");
+    }
+
+    public void LoadShopScene()
+    {
+        SceneManager.LoadScene("06_ShopScene");
+    }
 }
