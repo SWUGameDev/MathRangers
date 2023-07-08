eda8055e16a1ec068ae706c5edf594a2be370b4e
Kim YeonMo <ymthebest57@gmail.com>
2023-07-08 23:52:03 +0900

    🐛 Fix | time scale 버그 수정

--- Assets/Scripts/BossBattle/UI/BossSceneStop.cs
+++ Assets/Scripts/BossBattle/UI/BossSceneStop.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class BossSceneStop : MonoBehaviour
 {
     [SerializeField] GameObject stopPanel;
     [SerializeField] GameObject backSpace;
     [SerializeField] TextMeshProUGUI headerText;
+    [SerializeField] private SceneController sceneController;
 
+    private void Awake()
+    {
+        if(Time.timeScale == 0)
+        {
+            // TO DO : 타임 스케일 버그
+            Time.timeScale = 1;
+            Debug.Log("time scale log");
+        }
+    }
 
     public void GameStop()
     {
@@ -24,6 +35,14 @@ public class BossSceneStop : MonoBehaviour
         backSpace.SetActive(false);
         Time.timeScale = 0f;
         headerText.text = "게임 종료";
+
+    }
+
+    public void ExitGameScene()
+    {
+        Time.timeScale = 1f;
+        SoundManager.Instance.ChangeBackgroundAudioSource(backgroundAudioSourceType.BGM_MAIN);
+        sceneController.LoadMainScene();
     }
 
     public void PressBackspace()
