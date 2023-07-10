45c1ac7095f551d5d9a220d38adc0eaf907e5a3b
신은주 <slew61@naver.com>
2023-07-10 23:10:56 +0900

    🔨 Fix | 보스씬 남은 시간 UI 추가 (05:00)

--- Assets/Scripts/BossBattle/UI/BossSceneUIManager.cs
+++ Assets/Scripts/BossBattle/UI/BossSceneUIManager.cs
@@ -1,8 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public partial class BossSceneUIManager : MonoBehaviour
 {
+    [SerializeField] private TMP_Text limitTimeText;
 
+    [SerializeField] private float limitTimeSeconds;
+
+    //TODO : 시간 나면 로직 분리하기
+    private void Update() {
+
+        this.limitTimeSeconds -= Time.deltaTime;
+
+        if(this.limitTimeSeconds>=0)
+        {
+            //TODO : GameEnd 호출하기
+        }
+
+        TimeSpan time = TimeSpan.FromSeconds(this.limitTimeSeconds);
+        this.limitTimeText.text = time.ToString(@"mm\:ss");
+    }
+
+    private void Start() {
+        
+    }
 }
