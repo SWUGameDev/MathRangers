013f9c923501b00ba7082aae59b37b12a1f515ec
Kim YeonMo <ymthebest57@gmail.com>
2023-07-24 03:00:39 +0900

    ✨ Add | Run 플레이어 이동, 맵 이동, 치즈 먹는 기능

---
+++ Assets/Scripts/Utility/Define.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum MouseEvent
+    {
+        Press,
+        Click,
+    }
+    public enum CameraMode
+    {
+        QuarterView,
+    }
+}
