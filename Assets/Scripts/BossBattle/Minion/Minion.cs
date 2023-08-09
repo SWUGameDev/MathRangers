f18b04c8964a8429e5443a9ae2187cd45fcf7f7f
Kim YeonMo <ymthebest57@gmail.com>
2023-08-09 23:15:02 +0900

    🔨 Fix | 네임 스페이스 수정

--- Assets/Scripts/BossBattle/Minion/Minion.cs
+++ Assets/Scripts/BossBattle/Minion/Minion.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing.Text;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public partial class Minion : MonoBehaviour
 {
