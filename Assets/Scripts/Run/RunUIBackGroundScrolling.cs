c5f1d6b9df316a26744af0a4dede698791b54a4c
Kim YeonMo <ymthebest57@gmail.com>
2023-08-09 22:58:40 +0900

    🐛 Fix | 빌드 오류 수정

--- Assets/Scripts/Run/RunUIBackGroundScrolling.cs
+++ Assets/Scripts/Run/RunUIBackGroundScrolling.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Localization.Plugins.XLIFF.V12;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class RunUIBackGroundScrolling : MonoBehaviour
 {
