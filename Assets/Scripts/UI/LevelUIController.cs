e84b42e9daeeb31b9a9dce988f4b12c5ec6d0ff5
신은주 <slew61@naver.com>
2023-08-26 17:58:58 +0900

    🔨 Add | 마이페이지 내 UI 그래프 구현 (1)

--- Assets/Scripts/UI/LevelUIController.cs
+++ Assets/Scripts/UI/LevelUIController.cs
@@ -12,6 +12,10 @@ public class LevelUIController : MonoBehaviour
 
     [SerializeField] private Slider expSlider;
 
+    private int maxQuestionCount = 8;
+
+    private int[] countForLevelUp = new int[]{0,35,120,400,700}; 
+
     void Start()
     {
         UserGameResultInfoManager.OnUserGameResultInfoInitialized -= SetLevelUIData;
@@ -21,6 +25,28 @@ public class LevelUIController : MonoBehaviour
     private void SetLevelUIData(List<GameResultInfo> gameResultInfos)
     {
         UserGameResultInfoManager.OnUserGameResultInfoInitialized -= SetLevelUIData;
+
+        int count = gameResultInfos.Count * this.maxQuestionCount;
+        int level = 0;
+
+        for(int index = 0;index<this.countForLevelUp.Length;index++)
+        {
+            if(count<this.countForLevelUp[index])
+            {
+                level = index;
+                break;
+            }
+        }
+
+        this.levelText.text = $"Lv {level}";
+
+        // 최고 레벨일 경우 예외처리 따로 필요함
+        if(level<countForLevelUp.Length)
+        {
+            this.expText.text = $"{count}/{this.countForLevelUp[level]}";
+
+            this.expSlider.value = count/(float)this.countForLevelUp[level];
+        }
 
     }
 
